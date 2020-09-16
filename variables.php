2c9e1e0b71f90fbf2b304d379d69419d869b6b96
akpe2003 <aes_akpe@yahoo.ca>
2020-09-17 00:36:42 +0100

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Series2, Variables!</title>
+</head>
+<body>
+<?php      
+$var1=10;
+echo $var1;
+$var2="Hello World!";
+echo "<br />";
+echo $var2;
+echo "<br />";
+$my_variable="Hello World!";
+$my_Variable="Hello World Again!";
+echo $my_Variable;
+echo "<br />";
+$var1=100;
+echo $var1;
+?>
+</body>
+</html>
