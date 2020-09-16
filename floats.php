717b62ec83daeb91ec44dea3c2d8137f182a28f1
akpe2003 <aes_akpe@yahoo.ca>
2020-09-17 00:32:31 +0100

    Add files via upload

---
+++ floats.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Series3, Floating points/Decimals</title>
+</head>
+<body>
+<?php      
+$var1=3.14;
+echo "<br />";
+echo 4/3;
+echo "<br />";
+$myfloat=3.14;
+echo "<br />";
+echo round($myfloat,1);
+echo "<br />";
+echo ceil($myfloat);
+echo "<br />";
+echo floor($myfloat);
+?>
+</body>
+</html>
