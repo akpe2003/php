e3d98ceef8729418afa238a66e073f8b0225e200
akpe2003 <aes_akpe@yahoo.ca>
2020-09-17 00:32:05 +0100

    Add files via upload

---
+++ 00918-AISOSA.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+<html lang="en">
+<head>
+<title>HNGi7 Intern</title>
+<meta charset="utf-8" />
+</head>
+<body>
+<?php
+$full_name="Aisosa Akpevweoghene Ogheneowho Ugono";
+$id="HNG-00918";
+$language="PHP";
+$email="akpeugono@gmail.com";
+echo "Hello World!, this is $full_name with HNGi7 Internship ID $id using the language $language with e-mail address: $email for stage 2 task.";
+?>
+</body>
+</html>
