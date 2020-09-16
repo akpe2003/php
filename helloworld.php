08c73c4cf1a70ffacd54771959e02a592a60304e
akpe2003 <aes_akpe@yahoo.ca>
2020-09-17 00:33:05 +0100

    Add files via upload

---
+++ helloworld.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Series1, Hello World!</title>
+</head>
+<body>
+<?php echo ("Hello World!"); ?><br />
+<?php print ("Hello World!"); ?><br />
+<?php echo ("Hello " . " World!"); ?><br />
+<?php echo (2+3); ?>
+</body>
+</html>
