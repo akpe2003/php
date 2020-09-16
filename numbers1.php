0df89d55c65d709adfbe2020733272a681196ce0
akpe2003 <aes_akpe@yahoo.ca>
2020-09-17 00:33:53 +0100

    Add files via upload

---
+++ numbers1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Series3, Numbers!</title>
+</head>
+<body>
+<?php      
+$var1=3;
+$var2=4;
+echo ((1 + 2+ $var1) * $var2) / 2-5 ;
+echo "<br />";
+$var2 +=4;
+echo $var2;
+echo "<br />";
+$var2 -=4;
+echo $var2;
+echo "<br />";
+$var2 *=3;
+echo $var2;
+echo "<br />";
+$var2 /=4;
+echo $var2;
+echo "<br />";
+?>
+</body>
+</html>
