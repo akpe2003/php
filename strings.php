13b87dbf2c9534d687953da5ba3976514a9fb76e
akpe2003 <aes_akpe@yahoo.ca>
2020-09-17 00:35:26 +0100

    Add files via upload

---
+++ strings.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Series3, Strings!</title>
+</head>
+<body>
+<?php      
+echo "Hello World!<br />";
+echo 'Hello World<br />';
+$my_variable="Hello World";
+echo $my_variable;
+echo "<br />";
+echo $my_variable . " Again!";
+?>
+<br />
+<?php
+echo "$my_variable Again"; 
+echo "<br />";
+echo "{$my_variable} Again"
+?>
+</body>
+</html>
